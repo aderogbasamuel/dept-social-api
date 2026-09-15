b721d2a7f7db48f0d87a72ec2607ec9b67cba9dd
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2026-09-16 00:20:07 +0100

    add

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,10 @@ const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 const upload = require("../middleware/upload");
 const { updateUsername, uploadAvatar } = require("../controllers/userController");
-
+const {
+getPostsByUser
+} = require("../controllers/profileController");
 router.patch("/username", protect, updateUsername);
 router.post("/avatar", protect, upload.single("avatar"), uploadAvatar);
-
+router.get("/:id/", getPostsByUser);
 module.exports = router;
